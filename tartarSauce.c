8887feb60cfb9db07b58f24bce994e9b979d1882
scvif <jinbum2137@gmail.com>
2022-12-20 11:39:07 +0900

    변수 설정

---
+++ tartarSauce.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <time.h>
+
+
+int main() {
+	srand(time(NULL));
+
+	int score = 1;
+	int a, b, yeonsanja, correct, answer;
+	char yeonsanjaDisplay;
+	
+}
