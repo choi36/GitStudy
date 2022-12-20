42301f3e498d87d1b6d85d0ea56fa403a669ccb8
scvif <jinbum2137@gmail.com>
2022-12-20 11:40:08 +0900

    연산자 랜덤 설정 / 문제 만들기

--- tartarSauce.c
+++ tartarSauce.c
@@ -10,5 +10,31 @@ int main() {
 	int score = 1;
 	int a, b, yeonsanja, correct, answer;
 	char yeonsanjaDisplay;
-	
+	while (score <= 10) {
+		a = rand() % 20 + 1;
+		b = rand() % 20 + 1;
+		yeonsanja = rand() % 4 + 1;
+
+		if (yeonsanja == 1) {
+			correct = a + b;
+			yeonsanjaDisplay = '+';
+		}
+		else if(yeonsanja == 2) {
+			correct = a - b;
+			yeonsanjaDisplay = '-';
+		}
+		else if (yeonsanja == 3) {
+			correct = a * b;
+			yeonsanjaDisplay = '*';
+		}
+		else if (yeonsanja == 4) {
+			correct = a / b;
+			yeonsanjaDisplay = '/';
+		}
+
+		printf("[%d/10] %d %c %d = ", score, a, yeonsanjaDisplay, b);
+		scanf("%d", &answer);
+
+		
+	}
 }
